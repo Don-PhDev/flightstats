cee08c45bffdf48e58582de246b4eb37145db403
Don Forrest <forrest@philippinedev.com>
2022-04-30 10:09:24 +0800

    Use parameterize

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -4,7 +4,7 @@ class Airline < ApplicationRecord
   before_create :slugify
 
   def slugify
-    self.slug = name.downcase.gsub(" ", "-")
+    self.slug = name.parameterize
   end
 
   def avg_score
